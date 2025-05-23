b5b5a91eb80406d439572959bcf0eec4a67c4c6c
Simon Gadient <simon.gadient@oniva.events>
2025-05-23 15:00:17 +0200

    Update readme

--- Resources/Private/Scripts/config-templates/config.php
+++ Resources/Private/Scripts/config-templates/config.php
@@ -4,6 +4,7 @@ include(dirname(__FILE__) . '/../../../Packages/Libraries/simplesamlphp/simplesa
 
 $config['metadatadir'] = dirname(__FILE__) . '/../metadata/';
 $config['loggingdir'] = dirname(__FILE__) . '/../../../Data/Logs/';
+$config['cachedir'] = dirname(__FILE__) . '/../../../Data/Temporary/' . getenv('FLOW_CONTEXT') . '/Cache/SimpleSamlPhp/';
 $config['datadir'] = dirname(__FILE__) . '/../../../Data/';
 
 // TODO: Overwrite config here
